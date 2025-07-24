8fcb49ab93c500da8957c2bec6aed96217d32437
KavyaSharma112 <153850499+KavyaSharma112@users.noreply.github.com>
2025-07-24 23:39:37 +0530

    episode-03

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 #  Namaste React <3
+
+# Parcel
+- Dev Build
+- Local server
+- HMR = Hot Module Reload
+- File Watching Algorithm - written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
--- app.js
+++ app.js
@@ -1,41 +1,27 @@
-// const Parent = React.createElement( 
-//     "div",
-//     { id : "Parent"},
-//     React.createElement(
-//         "div",
-//         { id : "children"},
-//         [
-//             React.createElement("h1", {}, "I am an h1 tag !"),
-//             React.createElement("h2", {}, "I am an h2 tag !")
-//         ]
-//     )
-// );
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const Parent = React.createElement(
-    "div",
-    { id : "Parent"},
-    [
-        React.createElement(
-            "div",
-            { id : "child1"},
-            [
-                React.createElement("h1", {}, "Kavya you can do it !"),
-                React.createElement("h2", {}, "Keep going !")
-            ]
-        ),
-        React.createElement(
-            "div",
-            { id : "child2"},
-            [
-                React.createElement("h1", {}, "Google awaits you !"),
-                React.createElement("h2", {}, "Stanford University has accepted you!")
-            ]
-        )
-    ]
-)
+// JSX (transpiled before it reaches the JS engine) - Parcel - Babel
+
+// JSX =>Babel transpiles it to => React.createElement => ReactElement-JS object =>HTMLElement(render)
+// jsx - HTML/XML like syntax
+
+const Title = () => (
+    <h1 className = "head">
+    Namaste React using JSX!
+    </h1>
+);
 
 
-console.log(Parent);
+// Functional React component 
+const HeadingComponent = () => (
+    <div id = "container">
+        {Title()}
+        <Title/>
+        <h1>Hello noggler! this is a react functional component</h1>
+    </div>
+    );
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(Parent);
+root.render(<HeadingComponent/>);
