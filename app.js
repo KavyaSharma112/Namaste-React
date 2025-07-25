84c55aa058d1d4c2660a861e041d4299b9061cbe
KavyaSharma112 <153850499+KavyaSharma112@users.noreply.github.com>
2025-07-25 23:23:07 +0530

    Started episode-04

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// JSX (transpiled before it reaches the JS engine) - Parcel - Babel
-
-// JSX =>Babel transpiles it to => React.createElement => ReactElement-JS object =>HTMLElement(render)
-// jsx - HTML/XML like syntax
-
-const Title = () => (
-    <h1 className = "head">
-    Namaste React using JSX!
-    </h1>
-);
-
-
-// Functional React component 
-const HeadingComponent = () => (
-    <div id = "container">
-        {Title()}
-        <Title/>
-        <h1>Hello noggler! this is a react functional component</h1>
+const Header = () => {
+    return (<div className="header">
+        <div className="logo">
+            <img src = "https://media.istockphoto.com/id/2176185103/vector/food-truck-emblem-or-logo-restaurant-diner-street-service-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=-zxcoSiDfH7gRiBvl-1lD1qqMiWW8zYM9jPl_FAj-Eg="/>
+        </div>
+        <div className="nav-items">
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li><img src = "https://media.istockphoto.com/id/1127711786/vector/supermarket-cart-icon-vector-on-white-background-supermarket-ca.jpg?s=612x612&w=0&k=20&c=a-SUKULPzZXCXbszSoqKOo5lEmUEe5iEVYHJOtEm_Ic="/></li>
+            </ul>
+        </div>
     </div>
     );
+}
 
+const AppLayout = () => {
+    return (<div className="app">
+        <Header/>
+
+    </div>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent/>);
+root.render(<AppLayout/>);
